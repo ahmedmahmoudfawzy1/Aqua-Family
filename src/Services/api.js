bcad1bab5fd5c3aa4a620c7368727ee74d8df49f
ahmedmahmoudfawzy1 <afaw6076@gmail.com>
2024-02-04 09:35:00 +0200

    loading

--- src/Services/api.js
+++ src/Services/api.js
@@ -1,3 +1,3 @@
-//  const apiLink = "https://aquafamily.onrender.com"
-const apiLink = "http://localhost:3004";
+const apiLink = "https://aquafamily.onrender.com";
+// const apiLink = "http://localhost:3004";
 export default apiLink;
